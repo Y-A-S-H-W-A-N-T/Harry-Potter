2bccb8bf91c62e347782b0a94123b02a4365908b
raoyashwant <raoyashwant132@gmail.com>
2024-03-03 02:09:46 +0530

    Setting up environment for reading pdf and created a layout for the webpage

---
+++ pages/Part_one/act_two.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+export default function ACT_ONE() {  
+
+  const [pageno,setPageno] = useState(1)
+  const [totalPages, setTotalPages] = useState(1)
+  const [left,setLeft] =useState(false)
+  const [right,setRight] =useState(true)  
+
+  const nextPage = ()=>{
+    setLeft(true)
+    pageno + 1 > totalPages ? setRight(false) : setPageno(pageno+1)
+  }
+  const previousPage = ()=>{
+    pageno - 1 < 1 ? setLeft(false) : setPageno(pageno-1)
+  }
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setTotalPages(numPages)
+  }
+
+  return (
+    <div>
+      <div style={{display: 'flex', cursor: 'pointer', justifyContent: 'center'}}>
+        {left && <div onClick={previousPage}>◀️</div>}
+        {right && <div onClick={nextPage}>▶️</div>}
+      </div>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <Document file='/test.pdf' onLoadError={console.error} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageno} height={300} width={500} renderAnnotationLayer={false} renderTextLayer={false}/>
+        </Document>
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Link from "next/link"
+import { useState } from "react"
+import style from '../styles/Home.module.css'
+import Back from '../public/harry.jpg'
+
+export default function Home() { 
+  
+  const [part1,setPart1] = useState(false)
+  const [part2,setPart2] = useState(false)
+
+  return (
+    <div className={style.back}>
+      <img src='./harry.jpg' className={style.harry}/>
+      <div className={style.potter}>
+        <h1 style={{justifyContent: 'center', display: 'flex'}}>HARRY POTTER AND THE CURSED CHILD</h1>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <div style={{display: 'block'}}>
+            <h1 onClick={()=>setPart1(!part1)} style={{paddingRight: '50px'}}>Part one</h1>
+            { part1 && <><Link href="/Part_one/act_one">ACT ONE</Link><br/>
+            <Link href="/Part_one/act_two">ACT TWO</Link></>}
+          </div>
+          <div style={{display: 'block'}}>
+            <h1  onClick={()=>setPart2(!part2)}>Part two</h1>
+            { part2 && <><Link href="/Part_two/act_three">ACT THREE</Link><br/>
+            <Link href="/Part_two/act_four">ACT FOUR</Link></>}
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
